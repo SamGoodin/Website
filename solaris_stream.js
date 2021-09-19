232be273caea7c1e9929da34d4794cb873e51a92
Sam Goodin <samuelgoodin@sbcglobal.net>
2021-09-19 16:32:49 -0400

    bug fix - now can reload stream with new source dynamically

--- solaris_stream.js
+++ solaris_stream.js
@@ -7,7 +7,7 @@ function onloadStream() {
 	
 	var SolarisStreamSrc = "";
 	
-	if (!window.localStorage.getItem("lastSolarisSrc")) {
+	if (!window.localStorage.getItem("lastSolarisSrc") || window.localStorage.getItem("lastSolarisSrc") == null) {
 		SolarisStreamSrc = prompt("Please enter the stream source"); 
 		window.localStorage.setItem("lastSolarisSrc", SolarisStreamSrc);
 	}
@@ -17,14 +17,30 @@ function onloadStream() {
 	
 	createSolarisStream(SolarisStreamSrc);
 	var streamSrcP = document.createElement("p");
-	streamSrcP.innerHTML = "Last source used: " + SolarisStreamSrc;
+	streamSrcP.id = "streamSrc";
+	streamSrcP.innerHTML = "Current source: " + SolarisStreamSrc;
 	document.getElementById("homepage").appendChild(streamSrcP);
 	
+	var resourceBtn = document.createElement("button");
+	resourceBtn.type = 'button';
+	resourceBtn.innerHTML = 'Use new source';
+	resourceBtn.onclick = getNewSource;
+	document.getElementById("homepage").appendChild(resourceBtn);
+	
+}
+
+function getNewSource() {
+	var newSrc = prompt("Please enter the stream source"); 
+	window.localStorage.setItem("lastSolarisSrc", newSrc);
+	document.getElementById("player").src = newSrc;
+	document.getElementById("streamSrc").innerHTML = "Current source: " + newSrc;
+
 }
 
 function createSolarisStream(source) {
 	var body = document.getElementById("homepage");
 	newiframe = document.createElement("iframe");
+	newiframe.id = "player";
 	newiframe.src = source;
 	newiframe.height = 400;
 	newiframe.width = 500;
